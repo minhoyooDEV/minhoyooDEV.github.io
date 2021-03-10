98db77279f16449f541f9d4e259ccdeb67ca6ef4
myno <minho.yoo.dev@gmail.com>
2021-03-10 20:12:37 +0900

    Install prettier

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # THANK YOU FOR YOUR VISITING !
 
-
 Updated 03-03-21
-- Setup environment (nextjs, githubpage with actions, tailwindcss)
+
+- Setup environment (nextjs, githubpage with actions, tailwindcss, typescript, prettier)
--- next.config.js
+++ next.config.js
@@ -1,21 +1,19 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 const isProd = process.env.NODE_ENV === "production";
 
-const {
-  ON_GITHUB_PAGES,
-} = process.env;
+const { ON_GITHUB_PAGES } = process.env;
 
-const basePath ="";
+const basePath = "";
 // const assetPrefix = ON_GITHUB_PAGES ? "https://cdn.statically.io/gh/minhoyooDEV/minhoyoodev.github.io/gh-pages/" : "";
-const assetPrefix = ''
+const assetPrefix = "";
 
 // https://dev.to/jameswallis/deploying-a-next-js-app-to-github-pages-24pn
 module.exports = (phase, { defaultConfig }) => {
-//   if (phase === PHASE_DEVELOPMENT_SERVER) {
-//     return {
-//       /* development only config options here */
-//     };
-//   }
+  //   if (phase === PHASE_DEVELOPMENT_SERVER) {
+  //     return {
+  //       /* development only config options here */
+  //     };
+  //   }
 
   return {
     basePath,
