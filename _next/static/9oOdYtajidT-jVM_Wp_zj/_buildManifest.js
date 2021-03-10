14e40942a6e9e0ece71c8b5fb9a4c43e222bb8d0
minhoyooDEV <minhoyooDEV@users.noreply.github.com>
2021-03-10 09:42:33 +0000

    deploy: 6a4ec24e68ca53d30a18c1ab459671a92607c3d9

---
+++ _next/static/9oOdYtajidT-jVM_Wp_zj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-792b9c96d4b218639d43.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ce621c9495c14332dd61.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/WTJSbLnz7AkW8rF8od00l/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fc50ddf22b716c7b6b76d.css","static\u002Fchunks\u002Fpages\u002Findex-2b5df12f2bec84bc5112.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-6ed71978a7385d99b56d.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
