60a387336002e6f4fbd4fcdf252303a53aa70234
myno <minho.yoo.dev@gmail.com>
2021-03-11 20:13:02 +0900

    Fix css system.

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
 module.exports = {
-  plugins:
-    [
-      "tailwindcss",
-      ...process.env.NODE_ENV === "production"
-      ? [
-          "postcss-flexbugs-fixes",
-          [
-            "postcss-preset-env",
-            {
-              autoprefixer: {
-                flexbox: "no-2009",
-              },
-              stage: 3,
-              features: {
-                "custom-properties": false,
-              },
-            },
-          ],
-        ]
-      : [
-        ],]
+  plugins: [
+    "tailwindcss",
+    ...(process.env.NODE_ENV === "production" ? [] : []),
+  ],
 };
